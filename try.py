b6d5a7e1ff4d4d8d42b02e55edac0323dce5dd02
dondees <58193147+dondees@users.noreply.github.com>
2020-03-10 21:29:06 +0100

    Add files via upload

---
+++ try.py
@@ -0,0 +1,4 @@
+def displayIntro():
+    print('''You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly 
+    and will share his treasure with you. The other dragon is greedy and hungry, and will eat you on sight.''')
+print()
